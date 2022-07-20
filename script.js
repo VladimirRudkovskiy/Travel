1e8dca0c41992624f18cbafcd456bfbea88adae6
Vladimir <rudkovskyvladimir21051@gmail.com>
2022-07-20 13:21:17 +0300

    home section

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar')
-let navbar = document.querySelector('.navbar')
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
 
 window.onscroll = () =>{
 	searchBtn.classList.remove('fa-times');
@@ -30,4 +31,13 @@ formBtn.addEventListener('click', () =>{
 
 formClose.addEventListener('click', () =>{
 	loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+	btn.addEventListener('click', () =>{
+		document.querySelector('.controls .active').classList.remove('active');
+		btn.classList.add('active');
+		let src = btn.getAttribute('data-src');
+		document.querySelector('#video-slider').src = src;
+	});
 });
