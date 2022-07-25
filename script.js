92647c48b9183c51f171678a259b80651c81cecc
Vladimir <rudkovskyvladimir21051@gmail.com>
2022-07-25 12:54:12 +0300

    review

--- script.js
+++ script.js
@@ -41,3 +41,23 @@ videoBtn.forEach(btn =>{
 		document.querySelector('#video-slider').src = src;
 	});
 });
+
+var swiper = new Swiper(".review-slider", {
+	spaceBetween: 20,
+	loop: true,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+	}
+});
